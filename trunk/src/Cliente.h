c60b78b50d053ebc413f7f65d2ed5a1dc4a565f7
tatoqac@gmail.com <tatoqac@gmail.com@7e5a14ec-3cf2-37fe-fe1e-88608d0336ab>
2010-08-28 20:53:54 +0000

    Prueba de comitt con una clase creada

---
+++ trunk/src/Cliente.h
@@ -0,0 +1,17 @@
+/*
+ * Cliente.h
+ *
+ *  Created on: 28/08/2010
+ *      Author: gaston
+ */
+
+#ifndef CLIENTE_H_
+#define CLIENTE_H_
+
+class Cliente {
+public:
+	Cliente();
+	virtual ~Cliente();
+};
+
+#endif /* CLIENTE_H_ */
