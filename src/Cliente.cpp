5d280dc7fad71b90a36d4af4d8aa2ed9dda56624
tatoqac <tatoqac@gmail.com>
2010-08-28 20:53:54 +0000

    Prueba de comitt con una clase creada

---
+++ src/Cliente.cpp
@@ -0,0 +1,17 @@
+/*
+ * Cliente.cpp
+ *
+ *  Created on: 28/08/2010
+ *      Author: gaston
+ */
+
+#include "Cliente.h"
+
+Cliente::Cliente() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Cliente::~Cliente() {
+	// TODO Auto-generated destructor stub
+}
