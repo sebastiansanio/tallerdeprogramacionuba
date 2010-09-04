20c2a3a9d326211eae36b9d1f4ce5a6343e54b18
tatoqac <tatoqac@gmail.com>
2010-09-04 15:18:01 +0000

    de a poco mejor

--- src/ServidorCliente.cpp
+++ src/ServidorCliente.cpp
@@ -1,7 +1,28 @@
 #include "ServidorCliente.h"
 
-ServidorCliente::ServidorCliente() {
-	// TODO Auto-generated constructor stub
+ServidorCliente::ServidorCliente(paraThreadsRecibidos* cliente) {
+	//creamos el socket
+	this->descriptorSocket=socket(AF_INET,SOCK_STREAM,0);
+	if(this->descriptorSocket==-1){
+		cout<<"Mal creado socket"<<endl;
+	}
+	else{
+		cout<<"Socket bien creado"<<endl;
+	}
+	this->cliente=cliente;
+
+}
+
+void ServidorCliente::interactuarConCliente(){
+	char data[MAXBYTES];
+	socklen_t leng=sizeof(data);
+	ssize_t valorRecive=recv(cliente->valorAcept,&data,leng,0);
+	if(valorRecive==-1){
+		cout<<"Mal recibido"<<endl;
+	}else{
+		data[valorRecive]='\0';
+		cout<<data<<endl;
+	}
 
 }
 
