bf9498b3b4d243c4568b187101fb8c8804ecb3bf
tatoqac <tatoqac@gmail.com>
2010-09-06 18:21:09 +0000

    las operaciones

--- src/S.cpp
+++ src/S.cpp
@@ -6,8 +6,34 @@ S::S() {
 }
 
 list<char*>* S::realizarOpearacion(list<char*>* operandos){
-	list<char*>* xml=new list<char*>();
-	return xml;
+	list<char*>* respuesta = new list<char*>();
+	list<char*>::iterator it=operandos->begin();
+	double suma=0;
+	char* sumaChar=new char[100];
+	char* sumandoChar;
+	double sumandoDouble;
+	while(operandos->size()>0){
+		if(operandos->front()=="sumando"){
+			operandos->pop_front();
+			sumandoChar=operandos->front();
+			operandos->pop_front();
+			if((!esUnNumero(sumandoChar))){//Corroborar que no hayan puesto por ej 4.3.2
+				it=respuesta->begin();
+				respuesta->insert(it,"Error");
+				it++;
+				respuesta->insert(it,"Alguno de los operandos de la suma no es un número");
+				return respuesta;
+			}
+			sumandoDouble=atof(sumandoChar);
+			suma+=sumandoDouble;
+		}
+	}
+	sprintf(sumaChar,"%d",suma);
+	it=respuesta->begin();
+	respuesta->insert(it,"Correcto");
+	it++;
+	respuesta->insert(it,sumandoChar);
+	return respuesta;
 }
 
 S::~S() {
