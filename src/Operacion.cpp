0fad36ff41bd5b5e96dffe24e8c1f39c39a03c19
tatoqac <tatoqac@gmail.com>
2010-09-15 02:20:53 +0000

    anda lo de esUnNumero

--- src/Operacion.cpp
+++ src/Operacion.cpp
@@ -23,12 +23,18 @@ bool Operacion::esUnNumero(char* numero){
 //		}
 //
 //	}
-//	bool esUnNumero=true;
-//	int i=0;
-//	while((esUnNumero)and(i<TAMANOVECTOROPERANDOS)){
-//		esUnNumero=((numero[i]>9)and(numero[i]=='.'));
-//	}
-	return true;
+	ostringstream sstream;
+	sstream << numero;
+	string numeroAux = sstream.str();
+	bool esUnNumero=true;
+	bool aparecioPunto=false;
+	int i=0;
+	while((esUnNumero)and(i<numeroAux.size())){
+		esUnNumero=(((numero[i]<='9')and(numero[i]>='0'))or((numero[i]=='.')and(!aparecioPunto)));
+		if(numero[i]=='.') aparecioPunto=true;
+		i++;
+		}
+	return esUnNumero;
 }
 
 list<string>* Operacion::realizarOpearacion(list<char*>* operandos){
