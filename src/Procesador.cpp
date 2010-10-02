fe9da24a5027e939428490503d03f8e6b0763d35
tatoqac <tatoqac@gmail.com>
2010-10-02 23:09:02 +0000

    no anda bien todavia el enviar un archivo

--- src/Procesador.cpp
+++ src/Procesador.cpp
@@ -3,11 +3,14 @@ using namespace std;
 #include <stdlib.h>
 #include <iostream>
 #include <stdio.h>
+#include <fstream>
 
 Procesador::Procesador(int i) {
 	this->parser=new ParserServidor();
 
 }
+
+
 
 char* Procesador::getRespuesta(char* xml){
 	ostringstream sstream;
@@ -28,6 +31,28 @@ char* Procesador::getRespuesta(char* xml){
 		case('D'):{res="S";operacion=new D();break;}
 		case('M'):{res="S";operacion=new M();break;}
 		case('R'):{res="S";operacion=new R();break;}
+		case('B'):{res="N";
+				// referencia al archivo
+				fstream  archivo;
+
+				//intenta abrir el archivo en modo lectura y binario
+				archivo.open("/home/gaston/workspace/TpTallerDeProgramacionI/quilmes.bmp", fstream::in | fstream::binary );
+				if(archivo.good()) cout<<"se abrio bien"<<endl;
+				unsigned int* tamano=new unsigned int;
+				char bm[2];
+				archivo.read((char*)bm,2);
+				archivo.read((char*)(tamano),sizeof(int));
+				archivo.close();
+				archivo.open("/home/gaston/workspace/TpTallerDeProgramacionI/quilmes.bmp", fstream::in | fstream::binary );
+				cout<<*tamano<<endl;
+				archivo.read((char*)respuesta,*tamano);
+				archivo.close();
+				delete tamano;
+				ostringstream sstream2;
+				sstream2 << respuesta;
+				string paraVerCuantoPesa2 = sstream.str();
+				return respuesta;
+				}//se requiere el bitmap
 		default:{
 			list<string>* conError=new list<string>();
 			list<string>::iterator it=conError->begin();
@@ -54,6 +79,7 @@ char* Procesador::getRespuesta(char* xml){
 }
 
 Procesador * Procesador::instanciaUnica;
+
 Procesador *Procesador::instancia(){
 		    if (!instanciaUnica)
 		    	instanciaUnica =new Procesador(1);
